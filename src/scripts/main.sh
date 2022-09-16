b46caadc46f87af181377dd4218632969f2ac130
mkotsollaris <mkotsollari@gmail.com>
2022-09-16 08:48:03 -0700

    sh

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -5,10 +5,10 @@ find . -type f -name "*.md" | while read mdFile; do
   B=$(date +'%Y-%d-%m');
   echo $(date -jf %Y-%d-%m $A +%s)
   echo $(date -jf %Y-%d-%m $B +%s)
-  DIFF=$((($(date -jf %Y-%d-%m "$B" +%s) - $(date -jf %Y-%d-%m "$A" +%s))/86400))
-  echo $mdFile $DIFF
-  if [ $DIFF>60 ]
-    then
-        exit 1
-    fi
+  # DIFF=$((($(date -jf %Y-%d-%m "$B" +%s) - $(date -jf %Y-%d-%m "$A" +%s))/86400))
+  # echo $mdFile $DIFF
+  # if [ $DIFF>60 ]
+  #   then
+  #       exit 1
+  #   fi
 done
