2afd7c5250e9202e356718f175b31b95e2704f0c
mkotsollaris <mkotsollari@gmail.com>
2022-09-16 08:20:28 -0700

    add readme-check command

---
+++ src/scripts/main.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+find . -type f -name "*.md" | while read mdFile; do
+  A=$(date -r $mdFile +'%Y-%d-%m');
+  B=$(date +'%Y-%d-%m');
+  DIFF=$(((`date -jf %Y-%d-%m "$B" +%s` - `date -jf %Y-%d-%m "$A" +%s`)/86400))
+  echo $mdFile $DIFF
+  if [ $DIFF>60 ]
+    then
+        exit 1
+    fi
+done
