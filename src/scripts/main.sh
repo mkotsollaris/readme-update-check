497f4a525f909f20d2373faefdd027d7b5bf72aa
mkotsollaris <mkotsollari@gmail.com>
2022-09-18 14:29:46 -0700

    try

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 find . -type f -name "*.md" | while read mdFile; do
-  A=$(git log -1 --pretty="format:%as" $mdFile +'%y%m%d')
-  B=$(date +'%y%m%d')
+  gitDate=$(git log -1 --pretty="format:%as" $mdFile)
+  B=$(date -d $gitDate +'%y%m%d')
   echo $A
   echo $B
   DIFF=$(( ($(date --date=$B +%s) - $(date --date=$A +%s) )/(60*60*24) ))
