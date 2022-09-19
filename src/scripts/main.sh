b067258be47e25f116cd08fd99650bd064cd4c46
Menelaos Kotsollaris <mkotsollaris@users.noreply.github.com>
2022-09-18 21:21:42 -0700

    Enhance parameters (#7)
    * try exit
    
    * params
    
    * test params
    
    * update main.sh
    
    * default DAYS_THRESHOLD
    
    * test
    
    * fix
    
    * test
    
    * fix
    
    * msg
    
    * fix if/else
    
    * experiment list bash
    
    * test
    
    * update main
    
    * fix
    
    * ci
    
    * added contains
    
    * fn intro
    
    * main
    
    * ci
    
    * bash script
    
    * test
    
    * bash
    
    * bash
    
    * if/else
    
    * bash
    
    * try
    
    * script
    
    * try
    
    * try
    
    * try
    
    * try
    
    * try
    
    * update
    
    * shoudContinue
    
    * contains
    
    * test
    
    * syntax
    
    * fix
    
    * fix
    
    * main
    
    * convert
    
    * fix
    
    * fix
    
    * fix
    
    * check
    
    * cci
    
    * fix
    
    * path
    
    * p

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -1,14 +1,37 @@
 #!/bin/bash
 
+echo "$IGNORED_FILES"
+echo "$DAYS_THRESHOLD"
+IGNORED_FILES=("$IGNORED_FILES")
+erroredFiles=()
 find . -type f -name "*.md" | while read mdFile; do
+  # check if mdFile is part of IGNORED_FILES
+  # removing the first ./ folder as this is CircleCi directory
+  modifiedMdFile="${mdFile}"
+  echo $modifiedMdFile
+  if [[ "${IFS}${IGNORED_FILES[*]}${IFS}" =~ "${IFS}${modifiedMdFile}${IFS}" ]]; then
+    echo "true"
+  else
+    echo "false"
+  fi
   gitDate=$(git log -1 --pretty="format:%as" $mdFile)
   B=$(date -d $gitDate +'%y%m%d')
   echo $A
   echo $B
   DIFF=$(( ($(date --date=$A +%s) - $(date --date=$B +%s) )/(60*60*24) ))
   echo $mdFile $DIFF
-  # if [ $DIFF>60 ]
-  #   then
-  #       exit 1
-  #   fi
+  echo $DIFF>"$DAYS_THRESHOLD"
+
+  if [ $DIFF -gt "$DAYS_THRESHOLD" ];
+  then
+    erroredFiles+=( $mdFile )
+  fi
 done
+
+ if [ ${#erroredFiles[@]} -eq 0 ]; then
+    echo "Your documentation is up to date. Good job!"
+  else
+      echo "Oops, something went wrong..."
+      echo "These files are not up to date: ${erroredFiles}. Consider updating them!"
+      exit 1
+  fi
