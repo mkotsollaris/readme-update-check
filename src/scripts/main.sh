de7114bfcdf42de5afb8d668ae4554f736ffb732
mkotsollaris <mkotsollari@gmail.com>
2022-09-18 19:34:24 -0700

    fix

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo Hello "${IGNORED_FILES}"
-echo Hello "${DAYS_THRESHOLD}"
+echo QQ "${DAYS_THRESHOLD}"
 find . -type f -name "*.md" | while read mdFile; do
   gitDate=$(git log -1 --pretty="format:%as" $mdFile)
   B=$(date -d $gitDate +'%y%m%d')
@@ -9,7 +9,7 @@ find . -type f -name "*.md" | while read mdFile; do
   DIFF=$(( ($(date --date=$A +%s) - $(date --date=$B +%s) )/(60*60*24) ))
   echo $mdFile $DIFF
   echo $DIFF>"$DAYS_THRESHOLD"
-  if [ $DIFF>"$DAYS_THRESHOLD" ]
+  if [ $DIFF>90 ]
     then
         exit 1
   fi
