2f1ef0c3a1ae1fa52c042389ac33884be3a4ff44
mkotsollaris <mkotsollari@gmail.com>
2022-09-18 20:53:19 -0700

    syntax

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -2,10 +2,11 @@
 
 echo "$IGNORED_FILES"
 echo "$DAYS_THRESHOLD"
+IGNORED_FILES="$IGNORED_FILES"
 erroredFiles=()
 find . -type f -name "*.md" | while read mdFile; do
   # check if mdFile is part of IGNORED_FILES
-  if [[ "$IGNORED_FILES" =~ " ${mdFile} " ]]; then
+  if [[ " ${IGNORED_FILES[*]} " =~ " ${mdFile} " ]]; then
     echo "gg!"
     continue
   fi
