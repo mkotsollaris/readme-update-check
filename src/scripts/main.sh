dd74d2a6ec82567cb87f0cb451d4c66e509e4ca6
mkotsollaris <mkotsollari@gmail.com>
2022-09-18 20:34:36 -0700

    try

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -29,12 +29,12 @@ find . -type f -name "*.md" | while read mdFile; do
   #   erroredFiles+=( $mdFile )
   # fi
 
-  if [ ${#erroredFiles[@]} -eq 0 ]; 
-  then
-    echo "Your documentation is up to date. Good job!"
-  else
-      echo "Oops, something went wrong..."
-      echo "These files are not up to date: ${erroredFiles}. Consider updating them!"
-      exit 1
-  fi
+  # if [ ${#erroredFiles[@]} -eq 0 ]; 
+  # then
+  #   echo "Your documentation is up to date. Good job!"
+  # else
+  #     echo "Oops, something went wrong..."
+  #     echo "These files are not up to date: ${erroredFiles}. Consider updating them!"
+  #     exit 1
+  # fi
 done
