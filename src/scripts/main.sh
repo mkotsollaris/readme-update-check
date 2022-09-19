e82e590dd4fe3e5d357efa351532830cac227875
mkotsollaris <mkotsollari@gmail.com>
2022-09-18 20:23:28 -0700

    bash

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -26,10 +26,12 @@ find . -type f -name "*.md" | while read mdFile; do
   echo $DIFF>"$DAYS_THRESHOLD"
 
   if [[ $DIFF -gt "$DAYS_THRESHOLD" ]]
+  then
     erroredFiles+=( $mdFile )
   fi
 
-  if [ ${#erroredFiles[@]} -eq 0 ]; then
+  if [ ${#erroredFiles[@]} -eq 0 ]
+  then
     echo "Your documentation is up to date. Good job!"
   else
       echo "Oops, something went wrong..."
