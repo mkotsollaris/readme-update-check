0c799d711868751bac134700ac84abcb77c91a42
mkotsollaris <mkotsollari@gmail.com>
2022-09-24 11:38:50 -0700

    cleanup

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 
-echo "$IGNORED_FILES"
-echo "$DAYS_THRESHOLD"
 IGNORED_FILES=("$IGNORED_FILES")
-erroredFiles=()
-## declare an array variable
 allFiles=()
+erroredFiles=()
+
 find . -type f -name "*.md" | while read mdFile; do
-  echo "mpika $mdFile"
   # check if mdFile is part of IGNORED_FILES
   # removing the first ./ folder as this is CircleCi directory
 
@@ -16,28 +13,28 @@ find . -type f -name "*.md" | while read mdFile; do
    
   if [[ "${IFS}${IGNORED_FILES[*]}${IFS}" =~ "${IFS}${mdFile}${IFS}" ]];
   then
-    echo "TODO continue here.."
     # skip if file is in $IGNORED_FILES
-    # continue
+    continue
   fi
+
   gitDate=$(git log -1 --pretty="format:%as" $mdFile)
   B=$(date -d $gitDate +'%y%m%d')
   DIFF=$(( ($(date --date=$A +%s) - $(date --date=$B +%s) )/(60*60*24) ))
-  echo "$mdFile has not beeing updated in $DIFF days"
-
+  
   if [ $DIFF -gt "$DAYS_THRESHOLD" ];
   then
     erroredFiles+=( $mdFile )
+  else
+    echo "INFO: $mdFile has not beeing updated in $DIFF days"
   fi
 done
-
-echo "LAST OUT"
-for i in "${allFiles[@]}"; do echo "CHECKARE ME: $i" ; done
 
 if [ ${#erroredFiles[@]} -eq 0 ]; then
   echo "Your documentation is up to date. Good job!"
 else
     echo "Oops, something went wrong..."
-    echo "These files are not up to date: ${erroredFiles}. Consider updating them!"
+    echo "These files are not up to date:"
+    for i in "${allFiles[@]}"; do echo "$i" ; done
+    echo "Consider updating them!"
     exit 1
 fi
